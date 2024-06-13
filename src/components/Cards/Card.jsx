69e5d3adaf6ffc830ee6d3c8d7734238de2723b5
EmilianoAprile <emilianoaprile77@icloud.com>
2024-06-13 17:46:13 +0200

    add > not posted blog

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,34 +1,57 @@
 import s from "./Card.module.css";
 
 const tagsStyle = {
-  'html': s.green,
-  'css': s.pink,
-  'js': s.yellow,
-  'php': s.blue,
-}
+  html: s.green,
+  css: s.pink,
+  js: s.yellow,
+  php: s.blue,
+};
 function Card({ title, content, image, tags, published }) {
-
   return (
     <>
-    {/* mostro solo i post che hanno published == true */}
-      {published && (
+      {/* mostro solo i post che hanno published == true */}
+      {published ? (
         <div className={s.card}>
           <div className={s.cardImage}>
-            <img src={image === '' ? "https://picsum.photos/200" : ''} alt="Titolo" />
+            <img
+              src={image === "" ? "https://picsum.photos/200" : ""}
+              alt="Titolo"
+            />
           </div>
           <div className={s.cardContent}>
             <h2 className="cardTitle">{title}</h2>
             <p className="cardText">{content}</p>
             <ul className={s.cardTags}>
               {tags.map((tag, i) => (
-                <li 
-                  className = {`${s.tag} ${tagsStyle[tag]}`} 
-                  key={i}>
-                    {`#${tag}`}
+                <li className={`${s.tag} ${tagsStyle[tag]}`} key={i}>
+                  {`#${tag}`}
                 </li>
               ))}
             </ul>
             <button className={s.cardBtn}>LEGGI DI PIÙ</button>
+          </div>
+        </div>
+      ) : (
+        <div className={`${s.card} ${!published ? `${s.notPosted}` : ""} `}>
+          {!published && <div className={s.notPostedText}>Post non pubblicato</div>}
+
+          <div className={s.cardImage}>
+            <img
+              src={image === "" ? "https://picsum.photos/200" : ""}
+              alt="Titolo"
+            />
+          </div>
+          <div className={s.cardContent}>
+            <h2 className="cardTitle">{title}</h2>
+            <p className="cardText">{content}</p>
+            <ul className={s.cardTags}>
+              {tags.map((tag, i) => (
+                <li className={`${s.tag} ${tagsStyle[tag]}`} key={i}>
+                  {`#${tag}`}
+                </li>
+              ))}
+            </ul>
+            <button className={`${!published ? `${s.d_none}` : `${s.cardBtn}`} `}>LEGGI DI PIÙ</button>
           </div>
         </div>
       )}
