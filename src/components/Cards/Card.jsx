204d7e8c50e2b45d546a3383eac634c64d534095
EmilianoAprile <emilianoaprile77@icloud.com>
2024-06-13 14:33:59 +0200

    add > props

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,21 +1,18 @@
 import cardStyle from "./Card.module.css";
 
-function Card({cardProps}) {
-
+function Card({ title, content, image, tags, published }) {
   return (
     <>
-      {cardProps.map((card) => (
-        <div className={cardStyle.card} key={card.id}>
-          <div className={cardStyle.cardImage}>
-            <img src={card.image} alt="" />
-          </div>
-          <div className={cardStyle.cardContent}>
-            <h2 className="cardTitle">{card.title}</h2>
-            <p className="cardText">{card.text}</p>
-            <button className={cardStyle.cardBtn}>LEGGI DI PIÙ</button>
-          </div>
+      <div className={cardStyle.card}>
+        <div className={cardStyle.cardImage}>
+          <img src={image} alt="" />
         </div>
-      ))}
+        <div className={cardStyle.cardContent}>
+          <h2 className="cardTitle">{title}</h2>
+          <p className="cardText">{content}</p>
+          <button className={cardStyle.cardBtn}>LEGGI DI PIÙ</button>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,28 @@
-import Card from "../Cards/Card"
-import mainStyle from './Main.module.css'
-import posts from '../../posts.js'
-
+import Card from "../Cards/Card";
+import mainStyle from "./Main.module.css";
+import posts from "../../posts.js";
 
 function Main() {
-    return (
-      <>
-        <main className={mainStyle.pageMain}>
-          <div className="container">
-            <div className={mainStyle.cards}>
-              <Card cardProps={posts}/>
-            </div>
+  return (
+    <>
+      <main className={mainStyle.pageMain}>
+        <div className="container">
+          <div className={mainStyle.cards}>
+            {posts.map((p) => (
+              <Card
+                key={p.id}
+                title={p.title}
+                content={p.content}
+                image={p.image}
+                tags={p.tags}
+                published={p.published}
+              />
+            ))}
           </div>
-        </main>
-      </>
-    )
-  }
-  
-  export default Main
-  
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Main;
